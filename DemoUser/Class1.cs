9e787229ba988ed198129f31265181f3419ac2cf
squallySP <lihaofu@gmail.com>
2015-03-25 15:43:50 +0800

    test

---
+++ DemoUser/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoUser
+{
+    public class User
+    {
+        public User(string userEmail)
+        {
+            if (userEmail == null)
+                throw new ArgumentNullException();
+
+            m_email = userEmail;
+        }
+
+        private string m_email;
+    }
+}
---
+++ UserTest/Class1Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using DemoUser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UserTest
+{
+    [TestClass]
+    public class Class1Test
+    {
+        [TestMethod]
+        public void ContructorTest()
+        {
+            string userEmail = "someone@somewhere.com";
+            User target = new User(userEmail);
+
+            Assert.IsTrue(target != null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (ArgumentNullException))]
+        public void ContructorTestNull()
+        {
+            User target = new User(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ContructorTestEmpty()
+        {
+            User target = new User("");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ContructorTestBlank()
+        {
+            User target = new User("           ");
+        }
+    }
+}
